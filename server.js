e738f9c2092eef108c0bedae847217e7f2002bd6
arq-gabo <jgabrielguerral@gmail.com>
2020-12-29 22:12:02 -0400

    [FEATURE]Peticiones HTTP y recibiendo información desde el cliente con Body y Query

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(router);
 
-app.use("/", function(req, res){
-    res.send("Hola");
-})
+// app.use("/", function(req, res){
+//     res.send("Hola");
+// })
+
+
+router.get("/message", function(req, res){
+    res.send("Lista de Mensajes")
+});
+
+router.post("/message", function(req, res){
+    res.send("Mensaje Añadido")
+});
+
+router.delete("/message", function(req, res){
+    console.log(req.body);
+    console.log(req.query);
+    res.send("Mensaje Borrado");
+});
+
 
 app.listen(3000);
 console.log("la aplicación esta escuchando en http://localhost:3000");
